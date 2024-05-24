c22e291149bb04e525bbe069c680709c5904b706
Sushma Kolli <sushmakolli1510@gmail.com>
2024-05-23 20:41:18 -0400

    commit

--- src/main/java/com/java/emsbackend/controller/EmployeeController.java
+++ src/main/java/com/java/emsbackend/controller/EmployeeController.java
@@ -8,13 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin("*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
     private EmployeeService employeeService;
-@PostMapping("/save")
+@PostMapping
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
         EmployeeDto savedEmployee= employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(savedEmployee,HttpStatus.CREATED);
